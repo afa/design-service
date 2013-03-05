8793028c09c3bf2113ebc78995f22a2ebbb17ba6
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-03-05 16:35:14 +0400

    added inflection to properly singularize address

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,13 +1,15 @@
 # Be sure to restart your server when you modify this file.
 
+ActiveSupport::Inflector.inflections do |inflect|
 # Add new inflection rules using the following format
 # (all these examples are active by default):
-# ActiveSupport::Inflector.inflections do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
 #   inflect.irregular 'person', 'people'
 #   inflect.uncountable %w( fish sheep )
-# end
+
+  inflect.singular(/(ss)$/i, '\1') # makes 'address'.singlarize work properly
+end
 #
 # These inflection rules are supported but not enabled by default:
 # ActiveSupport::Inflector.inflections do |inflect|
