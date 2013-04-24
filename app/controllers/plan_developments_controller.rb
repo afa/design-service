0c7969941b2f4a528079ddb0d0d91450a28a7aaf
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-04-25 01:03:15 +0400

    plan_development now works with strong_parameters

--- app/controllers/plan_developments_controller.rb
+++ app/controllers/plan_developments_controller.rb
@@ -1,12 +1,14 @@
 class PlanDevelopmentsController < InheritedResources::Base
-
+  def create
+    create!{ resource }
+  end
   def new
     @plan_development = PlanDevelopment.new.decorate
     @plan_development.build_family_composition
     @plan_development.build_comment
     @plan_development.build_attachment
   end
-  
+
 private
   def disable_form?
     action_name == 'show'
@@ -14,14 +16,13 @@ private
   helper_method :disable_form?
 
   def permitted_params
-    params.require(:plan_development).permit( :big_kitchen, :cabinet_needed, :cloakroom_needed,
-                                              :join_kitchen_with_living_room, :num_bedrooms, :num_builtin_closets,
-                                              :num_guests, :washing_room_needed, :num_plans,
-                                              :num_standpipes,
-                                              :address, :floor, :section,
-                                              family_composition_attributes: [:first_line, :second_line],
-                                              attachment_attributes: :file,
-                                              comment_attributes: :text
-                                            )
+    params.permit(plan_development: [:big_kitchen, :cabinet_needed, :cloakroom_needed,
+                                      :join_kitchen_with_living_room, :num_bedrooms, :num_builtin_closets,
+                                      :num_guests, :washing_room_needed, :num_plans,
+                                      :num_standpipes,
+                                      :address, :floor, :section,
+                                      family_composition_attributes: [:first_line, :second_line],
+                                      attachment_attributes: :file,
+                                      comment_attributes: :text ] )
   end
 end
--- app/models/plan_development.rb
+++ app/models/plan_development.rb
@@ -3,18 +3,8 @@ class PlanDevelopment < ActiveRecord::Base
   belongs_to :comment, class_name: 'ModerableText'
   belongs_to :attachment
   belongs_to :family_composition, class_name: 'ModerableText'
-  # attr_accessible :big_kitchen,
-  				  # :cabinet_needed,
-  				  # :cloakroom_needed,
-  				  # :join_kitchen_with_living_room,
-  				  # :num_bedrooms,
-  				  # :num_builtin_closets,
-  				  # :num_guests,
-  				  # :washing_room_needed,
-  				  # :num_plans,
-  				  # :attachment_attributes
-  accepts_nested_attributes_for :attachment, :comment
-  # attr_accessible :address_attributes, :comment_attributes
+
+  accepts_nested_attributes_for :attachment, :comment, :family_composition
 
   #validates :num_guests, numericality: true
   #validates :num_builtin_closets, numericality: true
