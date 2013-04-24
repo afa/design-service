bbae06e065a9a26d4f9fa792bd56d512ab92b1b4
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-04-24 16:48:14 +0400

    added active admin, root route should go before admin routes!

---
+++ app/admin/users.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register User do
+  index do
+    column :email
+    #column :current_sign_in_at
+    #column :last_sign_in_at
+    #column :sign_in_count
+    column :role
+    default_actions
+  end
+
+  form do |f|
+    f.inputs "User Details" do
+      #f.input :email
+      f.input :role
+    end
+    f.buttons
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 DesignService::Application.routes.draw do
+  root :to => 'welcome#index'
+  ActiveAdmin.routes(self)
 
   resources :clients
 
@@ -79,7 +81,7 @@ DesignService::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+
 
   # See how all your routes lay out with "rake routes"
 
