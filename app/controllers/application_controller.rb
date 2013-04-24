62561b3052eec65f8ada63447f103e4acab61fa3
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-04-24 17:00:13 +0400

    authentication of admin user gone away( probably cancan will work fine instead)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,18 @@ class ApplicationController < ActionController::Base
   def choose_layout
     user_signed_in? ? "registered_layout" : "unregistered_layout"
   end
+=begin  
+  def authenticate_admin_user!
+    authenticate_user! 
+    unless current_user#.admin?
+      flash[:alert] = "This area is restricted to administrators only."
+      redirect_to root_path
+    end
+  end
+   
+  def current_admin_user
+    return nil if user_signed_in? && !current_user.admin?
+    current_user
+  end
+=end
 end
