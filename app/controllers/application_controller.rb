f0f2c7bf963f72f2146cfe960b2c06ca24b7283e
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-05-07 21:43:20 +0400

    removed unused code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,20 +13,6 @@ class ApplicationController < ActionController::Base
   def choose_layout
     user_signed_in? ? "registered_layout" : "unregistered_layout"
   end
-=begin
-  def authenticate_admin_user!
-    authenticate_user!
-    unless current_user#.admin?
-      flash[:alert] = "This area is restricted to administrators only."
-      redirect_to root_path
-    end
-  end
-
-  def current_admin_user
-    return nil if user_signed_in? && !current_user.admin?
-    current_user
-  end
-=end
 
   # if user is logged in, return current_user, else return guest_user
   def current_or_guest_user
