d9e3fbe6e622a5d9769eed9cd698593d8f6364ca
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-05-08 13:20:49 +0400

    Don't set email for guest

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,7 +39,7 @@ class User < ActiveRecord::Base
   end
 
   def self.new_guest
-    u = new(name: "Гость", email: "guest_#{Time.now.to_i}#{rand(99)}@example.com")
+    u = new(name: "Гость", email: "")
     u.role = 'guest'
     u.save!(validate: false)
     u
