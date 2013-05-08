a6029f8eae295e281f257a2150b501d76d0fd249
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-05-08 20:41:54 +0400

    Helper render_attribute unnecessary because we have draper method for this purpose

--- app/decorators/plan_development_decorator.rb
+++ app/decorators/plan_development_decorator.rb
@@ -13,27 +13,10 @@ class PlanDevelopmentDecorator < Draper::Decorator
   def attachment_info
     source.attachment ? source.attachment.decorate.link : ''
   end
-  def render_attribute(attribute)
-    helpers.content_tag(:div, class: ['attribute', attribute]) do
-      result = ""
-      result << helpers.content_tag(:div, class: 'label') do
-        I18n.t("activerecord.attributes.#{source.class.model_name.underscore}.#{attribute}")
-      end
-      result << helpers.content_tag(:div, class: 'value') do
-        if column_for_attribute(attribute).try(&:type) == :boolean
-          send(attribute) ? 'Да' : 'Нет'
-        else
-          send(attribute).to_s
-        end
-      end
-      result.html_safe
-    end
-  end
   def kitchen_size
     PlanDevelopment::KITCHEN_SIZE.detect{|k,v| k == big_kitchen}.last
   end
   def num_guests_description
     num_guests ? PlanDevelopment::NUM_GUESTS[num_guests].last : 'Не указано'
   end
-
 end
