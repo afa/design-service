6c2851e270101c31882648f331550ecf008b07cd
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-05-09 12:01:28 +0400

    one attachment instantiated on creation, syntax errors fixed

--- app/controllers/replanning_endorsements_controller.rb
+++ app/controllers/replanning_endorsements_controller.rb
@@ -1,15 +1,18 @@
 class ReplanningEndorsementsController < InheritedResources::Base
   respond_to :html
+  def new
+    @replanning_endorsement.replanning_attachments.build
+    new!
+  end
   def create
-    @replanning_endorsement = ReplanningEndorsement.new(permitted_params)
     @replanning_endorsement.build_order do |order|
-      order.client = crrent_or_guest_user
+      order.client = current_or_guest_user
     end
     create!{ resource }
   end
 private
   def permitted_params
     params.permit(replanning_endorsement: [:developed_by_general_projector, :flat_area, :replanning_organization_licensed,
-                                      replanning_attachments_attributes: [:file] )
+                                      replanning_attachments_attributes: [:file] ] )
   end
 end
