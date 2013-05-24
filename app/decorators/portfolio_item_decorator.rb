d77d0ffb61fe0bc9e8d05610c7238e6d03624736
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-05-24 19:42:34 +0400

    decorators

--- app/decorators/portfolio_item_decorator.rb
+++ app/decorators/portfolio_item_decorator.rb
@@ -1,6 +1,16 @@
 class PortfolioItemDecorator < Draper::Decorator
   delegate_all
-
+  def rating
+    if rating >= 0
+      h.content_tag :div, class: 'positiveRating' do
+        "+#{source.rating}"
+      end
+    else
+      h.content_tag :div, class: 'negativeRating' do
+        source.rating
+      end
+    end
+  end
   # Define presentation-specific methods here. Helpers are accessed through
   # `helpers` (aka `h`). You can override attributes, for example:
   #
--- app/decorators/specialist_decorator.rb
+++ app/decorators/specialist_decorator.rb
@@ -1,16 +1,19 @@
 class SpecialistDecorator < Draper::Decorator
   delegate_all
 
+  def positive_feedback
+    source.positive_feedback
+  end
+  def negative_feedback
+    "-#{source.negative_feedback}"
+  end
+
   def toggle_like_text
-    source.like_from?(h.current_user) ? 'Убрать из избранного' : 'Занести в избранное'
+    source.like_from?(h.current_user) ? 'Убрать из избранных' : 'В избранные'
   end
 
   def link_to_profile_admin
     h.link_to profile.to_s, h.admin_user_path(profile)
-  end
-
-  def specialist_type_name
-    I18n.t 'specialist.titles.singular.' + Specialist::SPECIALIST_TYPE_NAME_BY_TYPE_ID[specialist_type]
   end
 
   def orders_info
@@ -18,7 +21,7 @@ class SpecialistDecorator < Draper::Decorator
   end
 
   def to_s
-    "#{specialist_type_name} #{source.to_s}"
+    "#{specialization} #{source.to_s}"
   end
 
   # Define presentation-specific methods here. Helpers are accessed through
