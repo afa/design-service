72eb428ad875c9a53bb67d6bfa5a98bae86b4e93
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-05-24 19:39:56 +0400

    portfolio item controller

--- app/controllers/portfolio_items_controller.rb
+++ app/controllers/portfolio_items_controller.rb
@@ -1,2 +1,12 @@
-class PortfolioItemsController < ApplicationController
+class PortfolioItemsController < InheritedResources::Base
+  load_and_authorize_resource
+  def new
+    @portfolio_item.build_photo_collection
+    @portfolio_item.photo_collection.photos.build
+    new!
+  end
+private
+  def permitted_params
+    params.permit(portfolio_item: [:title, photo_collection_attributes: { photos_attributes: [:photo, :_destroy]}])
+  end
 end
