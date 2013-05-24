807c90b8d8e34499c8e22cc65dbd9009295434f4
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-05-24 19:32:20 +0400

    specialist refactored

--- app/models/specialist.rb
+++ app/models/specialist.rb
@@ -1,35 +1,35 @@
 #encoding: utf-8
 class Specialist < ActiveRecord::Base
-  # extend Enumerize
-  # enumerize :specialist_type, in: [:architector, :engineer, :designer, :building_company, :building_brigade]
+  belongs_to :specialist_group
+
+  extend Enumerize
+  enumerize :specialization, in: [:designer, :architector, :engineer, :building_company, :building_brigade, :not_a_specialist]
 
   belongs_to :profile, class_name: 'User'
-  has_many :portfolio_photos
+  #has_many :portfolio_photos
   has_and_belongs_to_many :users_liked, class_name: 'User', join_table: 'specialist_likes'
   has_and_belongs_to_many :orders, class_name: 'Order', join_table: 'order_executors'
 
-  scope :by_specialist_type, ->(specialist_type) { where(specialist_type: SPECIALIST_TYPE_ID_BY_NAME[specialist_type]) }
+  scope :by_specialization, ->(specialization) { where(specialization: specialization) }
   scope :by_order, ->(order_id) { joins(:orders).where('orders.id' => order_id) }
 
-  accepts_nested_attributes_for :portfolio_photos, allow_destroy: true
+  #accepts_nested_attributes_for :portfolio_photos, allow_destroy: true
 
-  SPECIALIST_TYPE_ID_BY_NAME = Hash[ ["designers", "architectors", "engineers", "building_companies", "building_brigades"].map.with_index{|value,index| [value, index]} ]
-  SPECIALIST_TYPE_NAME_BY_TYPE_ID = SPECIALIST_TYPE_ID_BY_NAME.invert
+  delegate :full_name, :to_s, :messages,  to: :profile
 
-  delegate :full_name, :to_s, to: :profile
+  def rating; 666; end
+  def rating_count; 777; end
+  def positive_feedback; 10; end
+  def negative_feedback; 0; end
+  def number_of_completed_orders; 7; end
 
-  def rating
-    666
+  def description
+    "Ололо, я водитель НЛО!Ололо, я водитель НЛО!Ололо, я водитель НЛО!Ололо, я водитель НЛО!Ололо, я водитель НЛО!Ололо, я водитель НЛО!Ололо, я водитель НЛО!Ололо, я водитель НЛО!Ололо, я водитель НЛО!"
   end
-  def rating_count
-    777
-  end
-  def positive_feedback
-   '100500'
-  end
-  def negative_feedback
-    '0'
-  end
+
+  # def orders_in_portfolio_by_category(category)
+    # portfolio_by_category(category).first(2)
+  # end
 
   def like_from?(user)
     users_liked.where(id: user.id).count > 0
@@ -42,6 +42,7 @@ class Specialist < ActiveRecord::Base
   def delete_like_from(user)
     users_liked.delete(user)
   end
+
   def toggle_like_from(user)
     if like_from?(user)
       delete_like_from(user)
