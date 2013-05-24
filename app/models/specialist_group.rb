6d8c0d287e87062d12743a6d2d33f45629be48d7
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-05-24 19:31:31 +0400

    specialist groups are portfolio items through specialists

--- app/models/specialist_group.rb
+++ app/models/specialist_group.rb
@@ -1,4 +1,4 @@
 class SpecialistGroup < ActiveRecord::Base
   has_many :specialists
-  has_many :portfolio_items, include: :photo_collection
+  has_many :portfolio_items, through: :specialists, include: :photo_collection
 end
