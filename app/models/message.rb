5ac09f14af2af4528cca43b7c7d01278a596a0d5
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-05-24 19:29:27 +0400

    Message scopes

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,9 @@ class Message < ActiveRecord::Base
   belongs_to :to, class_name: 'User'
   belongs_to :text, class_name: 'ModerableText'
 
+  scope :by_user, ->(user_id){ where('messages.from_id = ? OR messages.to_id = ?', user_id, user_id) }
+  scope :received_by_user, ->(user_id){ where(to_id: user_id) }
+  scope :sent_by_user, ->(user_id){ where(from_id: user_id) }
+
   accepts_nested_attributes_for :text
 end
