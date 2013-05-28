7fb74a60761c264c78faec7806556031636dae3b
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-05-28 15:59:33 +0400

    migration fix

--- db/migrate/20130527172553_change_columns_from_integer_index_to_string.rb
+++ db/migrate/20130527172553_change_columns_from_integer_index_to_string.rb
@@ -2,10 +2,10 @@ class ChangeColumnsFromIntegerIndexToString < ActiveRecord::Migration
   def change
     change_table :plan_developments do |t|
       t.remove :num_guests
-      t.remove :kitchen_size
+#      t.remove :kitchen_size
       
       t.string :num_guests
-      t.string :kitchen_size
+#      t.string :kitchen_size
     end
   end
 end
--- db/migrate/20130527175154_remove_kitchen_size_change_type_ofbig_kitchen.rb
+++ db/migrate/20130527175154_remove_kitchen_size_change_type_ofbig_kitchen.rb
@@ -1,7 +1,7 @@
 class RemoveKitchenSizeChangeTypeOfbigKitchen < ActiveRecord::Migration
   def change
     change_table :plan_developments do |t|
-      t.remove :kitchen_size
+#      t.remove :kitchen_size
       t.remove :big_kitchen
       t.string :big_kitchen
     end
