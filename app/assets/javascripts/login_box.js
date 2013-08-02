49e003457fd962e94b2c8a06b2322bfada4d52db
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-08-02 14:58:44 +0400

    js fix indents, plan_development fix height of first block so that flat_area doesn't overflow container

--- app/assets/javascripts/login_box.js
+++ app/assets/javascripts/login_box.js
@@ -1,23 +1,22 @@
 $(function() {
-function black_content(){
-  $("#black_content").css("height",$(document).height());
-  $("#black_content").css("display","block");
-};
+  function black_content(){
+    $("#black_content").css("height",$(document).height());
+    $("#black_content").css("display","block");
+  };
 
 
-$(".registration").click(function(){
-black_content();
-$("#register").css("display","block");
-});
+  $(".registration").click(function(){
+    black_content();
+    $("#register").css("display","block");
+  });
 
-$(".login").click(function(){
-black_content();
-$("#login").css("display","block");
-});
+  $(".login").click(function(){
+    black_content();
+    $("#login").css("display","block");
+  });
 
-$(".close_fly_window").click(function(){
-//alert($(this).attr("class"));
-$(this).parent().css("display","none").parent().css("display","none");
-});
-
+  $(".close_fly_window").click(function(){
+    //alert($(this).attr("class"));
+    $(this).parent().css("display","none").parent().css("display","none");
+  });
 });
