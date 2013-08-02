294ad6a3f9109cda40f3a60f6fba065408cf9e0a
afa <afa.alone@gmail.com>
2013-08-02 17:00:55 +0400

    fix migrations

---
+++ db/migrate/20130428121000_add_client_str.rb
@@ -0,0 +1,10 @@
+class AddClientStr < ActiveRecord::Migration
+  def up                       
+    if !(PlanDevelopment.new.attributes.has_key?(:client) && PlanDevelopment.new.attributes.has_key?(:client_id))
+     add_column :plan_developments, :client, :string
+    end                        
+  end
+  def down                     
+   
+  end 
+end   
--- db/migrate/20130428121152_add_client_to_plan_developments.rb
+++ db/migrate/20130428121152_add_client_to_plan_developments.rb
@@ -1,7 +1,7 @@
 class AddClientToPlanDevelopments < ActiveRecord::Migration
-  def change
+  def change                   
     change_table :plan_developments do |t|
       t.references :client
-    end
+    end                        
   end
 end
