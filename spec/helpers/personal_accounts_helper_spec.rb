38166d5a307ce821c4e3d57ba5b61aa2f6d521f3
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-08-05 15:01:34 +0400

    fix singular-form controller name to plural-form (personal_accounts)

--- spec/helpers/personal_accounts_helper_spec.rb
+++ spec/helpers/personal_accounts_helper_spec.rb
@@ -10,6 +10,6 @@ require 'spec_helper'
 #     end
 #   end
 # end
-describe PersonalAccountHelper do
+describe PersonalAccountsHelper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
