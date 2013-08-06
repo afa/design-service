6a743e2599745551552c9389ec356274b074cf42
Thamira <thamira@mail.ru>
2013-08-06 20:44:23 +0400

    whoami fails on some machines (and rescue doesn't help). Purchase.rb modified only for example of several-files commit

--- config/initializers/paper_trail_version.rb
+++ config/initializers/paper_trail_version.rb
@@ -1,7 +1,10 @@
 class Version < ActiveRecord::Base
   if defined?(Rails::Console)
-    PaperTrail.whodunnit = "#{`whoami`.strip}: console"
+    #PaperTrail.whodunnit = "#{`whoami`.strip}: console"
+    PaperTrail.whodunnit = "#{'Anonymous'}: console"    
   elsif File.basename($0) == "rake"
-    PaperTrail.whodunnit = "#{`whoami`.strip}: rake #{ARGV.join ' '}"
+    #PaperTrail.whodunnit = "#{`whoami`.strip}: rake #{ARGV.join ' '}"
+    PaperTrail.whodunnit = "#{'Anonymous'}: rake #{ARGV.join ' '}"
   end
+
 end
