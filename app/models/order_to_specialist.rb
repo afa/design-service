9d5ecf313f438b0e0105fce74a23005e502668fd
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-08-07 18:20:56 +0400

    comment about model type

--- app/models/order_to_specialist.rb
+++ app/models/order_to_specialist.rb
@@ -1,3 +1,4 @@
+# Order of not specified type (typically is created from message or by phone call). So this class is a brother of PlanDevelopment et.al.
 class OrderToSpecialist < ActiveRecord::Base
   belongs_to :executor
   attr_accessible :description
