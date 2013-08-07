dcacad4f194a1cbf4a834a6ff1f4c29b60522665
afa <afa@afawork>
2013-08-08 01:02:31 +0400

    fix purchase references

---
+++ db/migrate/20130807160745_fix_purchases.rb
@@ -0,0 +1,17 @@
+class FixPurchases < ActiveRecord::Migration
+  def up
+   if (Purchase.new.attributes.has_key?(:orders_id))
+    remove_column :purchases, :orders_id
+   end
+   add_column :purchases, :order_id, :integer
+   if (Purchase.new.attributes.has_key?(:users_id))
+    remove_column :purchases, :users_id
+   end
+   add_column :purchases, :user_id, :integer
+  end
+
+  def down
+   remove_column :purchases, :user_id
+   remove_column :purchases, :order_id
+  end
+end
