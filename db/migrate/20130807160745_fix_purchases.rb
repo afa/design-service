1a4a5894c7f6fa94a478e3e078645c22bc70d3fa
afa <afa@afawork>
2013-08-08 01:15:27 +0400

    fix migration conditions

--- db/migrate/20130807160745_fix_purchases.rb
+++ db/migrate/20130807160745_fix_purchases.rb
@@ -1,10 +1,12 @@
 class FixPurchases < ActiveRecord::Migration
   def up
-   if (Purchase.new.attributes.has_key?(:orders_id))
+   if (Purchase.new.attributes.has_key?(:orders_id.to_s))
+    puts "rm orderss"
     remove_column :purchases, :orders_id
    end
    add_column :purchases, :order_id, :integer
-   if (Purchase.new.attributes.has_key?(:users_id))
+   if (Purchase.new.attributes.has_key?(:users_id.to_s))
+    puts "rm userss"
     remove_column :purchases, :users_id
    end
    add_column :purchases, :user_id, :integer
