daea902b49723bed375de92ec010f440060f0b32
afa <afa@afawork>
2013-08-15 01:24:55 +0400

    payments fix && migration workaround

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -5,15 +5,21 @@ class Payment < ActiveRecord::Base
  has_many :purchases
 
 
-  state_machine :state do
-   state :created
+  state_machine :state, :initial => :created do
+   state :created do
+   end
    state :requested
    state :payed
    state :failed
 
-   event :request, :created => :requested
-   event :ok, :requested => :payed
-   event :fail, :requested => :fail
-
+   event :request do
+    transition :created => :requested
+   end
+   event :ok do
+    transition :requested => :payed
+   end
+   event :fail do
+    transition :requested => :fail
+   end
   end
 end
--- db/migrate/20130812092456_plan_development_remove_client.rb
+++ db/migrate/20130812092456_plan_development_remove_client.rb
@@ -1,5 +1,7 @@
 class PlanDevelopmentRemoveClient < ActiveRecord::Migration
   def change
+   if PlanDevelopment.new.attributes.has_key?("client")
     remove_column :plan_developments, :client
+   end
   end
 end
