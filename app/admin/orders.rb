26abf210751a39b7f55fc0c867b4c5593e27fa2f
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-08-29 17:21:06 +0400

    make it possible to assign specialist an price for order

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,11 +1,18 @@
 ActiveAdmin.register Order do
   decorate_with OrderDecorator
   controller do
+    respond_to :json
     has_scope :by_client, as: :client, only: [:index]
     # polymorphic association
     # has_scope :by_executor, as: :executor, only: [:index]
     def permitted_params
-      params.permit(order: [{executor_ids: []}, :completion_status, :price])
+      params.permit(order: [:executor_id, :price])
+    end
+    def update
+      resource.assign_attributes(permitted_params[:order])
+      resource.executor_type = 'Specialist'
+      resource.save
+      redirect_to action: 'show'
     end
   end
 
