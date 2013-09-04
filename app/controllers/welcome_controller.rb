b6f004f5f0283d349f2dedd7f0c7e10f618415f4
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-09-04 15:39:51 +0400

    admin redirected on login

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,9 @@
 class WelcomeController < ApplicationController
   def index
     if user_signed_in?
-      if current_user.orders.count > 0
+      if current_user.admin?
+        redirect_to admin_root_path
+      elsif current_user.orders.count > 0
         redirect_to controller: 'profiles', action: 'orders'
       else
         render 'index_registered', layout: false
