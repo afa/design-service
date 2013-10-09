853ffa1f377cfeb8ba97556f81c45f616855c4ca
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-10-09 04:21:18 +0400

    order messages a bit refined

--- app/admin/messages.rb
+++ app/admin/messages.rb
@@ -1,6 +1,18 @@
 ActiveAdmin.register Message do
   decorate_with MessageDecorator
+  controller do
+    def permitted_params
+      params.permit(message: [:text])
+    end
+    protected :permitted_params
+  end
   show do
     render partial: 'message', locals: {message: resource}
+  end
+  form do |f|
+    f.inputs 'Текст сообщения' do
+      f.input :text
+    end
+    f.actions
   end
 end
