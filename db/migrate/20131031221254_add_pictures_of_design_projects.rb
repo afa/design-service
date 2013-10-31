c84638478b6e6b23681cd5d072827f411d18e158
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-11-01 02:18:06 +0400

    pictures for design projects (don't forget to migrate, seeding not neccesary)

---
+++ db/migrate/20131031221254_add_pictures_of_design_projects.rb
@@ -0,0 +1,12 @@
+class AddPicturesOfDesignProjects < ActiveRecord::Migration
+  def up
+    OrderCustomizer.where(typename: 'accesories').first_or_create(name: 'Аксессуары')
+    OrderCustomizer.find_each do |order_customizer|
+      order_customizer.image = "design_projects/#{order_customizer.typename}.jpg"
+      order_customizer.save
+    end
+  end
+
+  def down
+  end
+end
