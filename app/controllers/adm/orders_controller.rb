1f1db7cb63a6ff3c722f93699cb3a85cc01aa280
Panchuk <panchuk35@gmail.com>
2013-12-24 13:42:12 +0400

    ''

--- app/controllers/adm/orders_controller.rb
+++ app/controllers/adm/orders_controller.rb
@@ -1,7 +1,9 @@
 # coding: utf-8
 class Adm::OrdersController < Adm::ApplicationController
 
+	# страница заказов
 	def index
-
+		# берем пачку заказов, взятых по определенному фильтру
+		
 	end
 end
