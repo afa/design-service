157912e078088af976acd406529f4c75f129b48a
panchuk35 <panchuk35@gmail.com>
2013-12-27 01:54:29 +0400

    fix

--- app/controllers/adm/specialist_controller.rb
+++ app/controllers/adm/specialist_controller.rb
@@ -3,6 +3,7 @@ class Adm::SpecialistController < Adm::ApplicationController
 	def get_by_group_and_specialization
 		group_id = params[:specialist_id].to_i
 		specialization_id = params[:specialization_id].to_i
+		
 		Specialist.specialist_groups.where("specialist_groups.specialization_id = ? and ", 
 			specialization_id)
 	end
