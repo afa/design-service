a1a04b3d9aa8df41b86265c33121ad9c2a7f6c9f
Panchuk <panchuk35@gmail.com>
2013-12-30 17:21:08 +0400

    fix auth and add client

--- app/controllers/adm/group_controller.rb
+++ app/controllers/adm/group_controller.rb
@@ -1,23 +1,33 @@
 # coding: utf-8
 class Adm::GroupController < Adm::ApplicationController
 	def show
-		@id = params[:id].to_i
+		if get_current_user.moderator? || get_current_user.main_moderator? || get_current_user.admin?
+			@id = params[:id].to_i
 
-		@group_data = SpecialistGroup.find(@id)
-		@groups = Array.new
-		@groups << @group_data
+			@group_data = SpecialistGroup.find(@id)
+			@groups = Array.new
+			@groups << @group_data
 
-		@specialists = @group_data.specialists
+			@specialists = @group_data.specialists
+			@user_data = get_current_user
+		else
+			redirect_to root_path
+		end
 	end
 
 	def show_orders
-		@id = params[:id].to_i
+		if get_current_user.moderator? || get_current_user.main_moderator? || get_current_user.admin?
+			@id = params[:id].to_i
 
-		@group_data = SpecialistGroup.find(@id)
-		@groups = Array.new
-		@groups << @group_data
+			@group_data = SpecialistGroup.find(@id)
+			@groups = Array.new
+			@groups << @group_data
 
-		@specialists = @group_data.specialists
-		@orders = Order.where("specialist_group_id = ?", @id)
+			@specialists = @group_data.specialists
+			@orders = Order.where("specialist_group_id = ?", @id)
+			@user_data = get_current_user
+		else
+			redirect_to root_path
+		end
 	end
 end
--- app/controllers/adm/specialist_group_controller.rb
+++ app/controllers/adm/specialist_group_controller.rb
@@ -1,14 +1,18 @@
 # coding: utf-8
 class Adm::SpecialistGroupController < Adm::ApplicationController
 	def get_by_specialist
-		specialization_id = params[:specialist_group_id].to_i
-		groups = SpecialistGroup.where("specialization_id = ?", specialization_id)
+		if get_current_user.moderator? || get_current_user.main_moderator? || get_current_user.admin?
+			specialization_id = params[:specialist_group_id].to_i
+			groups = SpecialistGroup.where("specialization_id = ?", specialization_id)
 
-		data = render_to_string(partial: 'adm/order/groups',
-            locals: {groups: groups
-                    },
-            layout: false, formats: [:html], handlers: [:haml])
+			data = render_to_string(partial: 'adm/order/groups',
+	            locals: {groups: groups
+	                    },
+	            layout: false, formats: [:html], handlers: [:haml])
 
-		render :json => { data: data }
+			render :json => { data: data }
+		else
+			redirect_to root_path
+		end
 	end
 end
