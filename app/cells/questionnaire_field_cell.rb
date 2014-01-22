adc87f7641fee42adc48880af77b13661672b4e9
Panchuk <panchuk35@gmail.com>
2014-01-22 19:48:23 +0400

    fix

--- app/cells/questionnaire_field_cell.rb
+++ app/cells/questionnaire_field_cell.rb
@@ -1,29 +1,32 @@
+# coding: utf-8
 class QuestionnaireFieldCell < Cell::Rails
 
   def name_and_value(args)
-  	data = args[:data]
-  	@name = data.name
+  	@data = args[:data]
+  	@details = args[:details]
 
     render
   end
 
+  # нозвание поля, его значение обязательное
   def name_and_value_admin(args)
-  	data = args[:data]
-  	@name = data.name
+  	@data = args[:data]
+  	@details = args[:details]
   	
     render
   end
 
   def name_and_value_not_necessarily(args)
-  	data = args[:data]
-  	@name = data.name
+  	@data = args[:data]
+  	@details = args[:details]
 
     render
   end
 
+  # нозвание поля, его значение не обязательное
   def name_and_value_not_necessarily_admin(args)
-  	data = args[:data]
-  	@name = data.name
+  	@data = args[:data]
+  	@details = args[:details]
   	
     render
   end
--- app/models/questionnaire_field.rb
+++ app/models/questionnaire_field.rb
@@ -1,4 +1,14 @@
 # coding: utf-8
 class QuestionnaireField < ActiveRecord::Base
 	has_and_belongs_to_many :questionnaires
+
+	extend Enumerize
+  	enumerize :location, in: ['left', 'right', 'center_full']
+
+	# берем детали назначенного поля для шаблона анкеты
+	def details(questionnaire_id)
+		QuestionnaireField.select("questionnaire_fields_questionnaires.position, location, questionnaire_fields_questionnaires.value")\
+			.joins("inner join questionnaire_fields_questionnaires on questionnaire_fields_questionnaires.questionnaire_field_id = questionnaire_fields.id")\
+			.where("questionnaire_fields_questionnaires.questionnaire_field_id = ? and questionnaire_fields_questionnaires.questionnaire_id = ?", self.id, questionnaire_id).first
+	end
 end
