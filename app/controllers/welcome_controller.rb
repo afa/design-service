7ff1aa7c25acf05811b6881dfa4250ff9674b24c
afa <afa@afawork>
2014-02-05 18:39:22 +0400

    fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,13 @@
 class WelcomeController < ApplicationController
   def index
+    unless params[:need_recov].blank?
+     content_for :need_recov do
+      render :partial => 'layouts/unregistered/registration_form2'  #!!!
+     end
+    end
     if user_signed_in?
       if current_user.admin? || current_user.moderator?
-        redirect_to admin_root_path
+        redirect_to adm_index_index_path
       elsif current_user.orders.count > 0 || current_user.try(:specialist?)
         redirect_to controller: 'profiles', action: 'orders'
       else
